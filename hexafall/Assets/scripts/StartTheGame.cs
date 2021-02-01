5feb50d529fed4a430140b50aa70d2b592edf442
mohammad ali tajbakhsh <ali.tajbaksh@gmail.com>
2021-02-01 18:18:04 +0300

    hex
    first part

---
+++ hexafall/Assets/scripts/StartTheGame.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartTheGame : MonoBehaviour {
+	public Slider ColorSlider;
+	public Slider XrowSlider;
+	public Slider YrowSlider;
+	public Text ShowColorCount;
+	public Text ShowXrowCount;
+	public Text ShowYrowCount;
+
+	void Update () {
+
+		Floor.Xrow = (int)XrowSlider.value;
+        Floor.Yrow = (int)YrowSlider.value;
+        Floor.generatedcolor = new Color[(int)ColorSlider.value];
+		ShowColorCount.text = ColorSlider.value.ToString();
+		ShowXrowCount.text = XrowSlider.value.ToString();
+		ShowYrowCount.text = YrowSlider.value.ToString();
+	}
+	public void StartPlaying()
+    { 
+		SceneManager.LoadScene(1);
+    }
+	public void QuitTheGame()
+    {
+		Application.Quit();
+    }
+}
